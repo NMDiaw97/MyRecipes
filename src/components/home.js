9c3d73b0c1901366443ea7b452fa9e3e58fadcc7
Mamadou Dior DIAW <mamadou.diaw@sogem-omvs.org>
2022-05-24 21:00:54 +0000

    navbar

---
+++ src/components/home.js
@@ -0,0 +1,23 @@
+function Home() {
+    return (
+        <nav className="navbar">
+            <div className="container">
+                <div className="logo">logo</div>
+                <ul className="nav">
+                    <li className="item">
+                        <a href="#">Breakfast</a>
+                    </li>
+                    <li className="item">
+                        <a href="#">Lunch</a>
+                    </li>
+                    <li className="item">
+                        <a href="#">Dinner</a>
+                    </li>
+                </ul>
+                <div className="login">login</div>
+            </div>
+        </nav>
+    );
+}
+
+export default Home;
