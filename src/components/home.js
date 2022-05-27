98b149842dbc222d62065792ed1e85fb3c11cb9b
Mamadou Dior DIAW <mamadou.diaw@sogem-omvs.org>
2022-05-27 10:40:08 +0000

    responsivness

--- src/components/home.js
+++ src/components/home.js
@@ -1,9 +1,11 @@
 function Home() {
     return (
+
         <nav className="navbar">
             <div className="container">
                 <div className="logo">logo</div>
                 <ul className="nav">
+                    <li className="toggle"><i class="fas fa-bars"></i></li>
                     <li className="item">
                         <a href="#">Breakfast</a>
                     </li>
@@ -13,10 +15,16 @@ function Home() {
                     <li className="item">
                         <a href="#">Dinner</a>
                     </li>
+                    <li className="item">
+                        <a href="#">login</a>
+                    </li>
+
                 </ul>
-                <div className="login">login</div>
+
             </div>
         </nav>
+
+
     );
 }
 
