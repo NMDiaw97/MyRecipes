278f66f526ebce189a84bde5829ec5d293f6ca59
Ndeye Maty  DIAW <nmdiaw@gainde2000.sn>
2022-06-21 11:52:33 +0000

    hamburgerMenu done

--- src/components/home.js
+++ src/components/home.js
@@ -1,20 +1,31 @@
 import { useState } from "react";
 
-const Item = (props) =>{
-    const itemList = props.items.map((skill)=> <li className="item" key={skill}>{skill}</li>)
-    return itemList
-}
 
 function Home() {
-    return (
+   
+    const [toggle, setToggle] = useState(true);
 
+    const hamburgerMenu = () => {
+        setToggle(!toggle)
+    }
+   
+    return (
         <nav className="navbar">
-                <ul className="nav">
-                    <li className="toggle"><i className="fas fa-bars"></i></li>
-                    <li className="logo">
-                        <a href="/accueil">logo</a>
+            <h3 className="logo">logo</h3>
+            <h2 className="toggle"><i className={toggle ? 'fas fa-bars' : 'fas fa-times'} onClick={hamburgerMenu}></i></h2>
+                <ul className={toggle? 'navClosed': 'nav'}>
+                    <li className="item">
+                        <a href="/accueil">Breakfast</a>
                     </li>
-                    <Item items={['Breakfast','Lunch','Dinner','login']}/>
+                    <li className="item">
+                        <a href="/accueil">Lunch</a>
+                    </li>
+                    <li className="item">
+                        <a href="/accueil">Dinner</a>
+                    </li>
+                    <li className="item">
+                        <a href="/accueil">login</a>
+                    </li> 
                 </ul>
            
         </nav>
