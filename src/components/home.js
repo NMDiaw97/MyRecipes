dc244880e6505029180918b16d887d7a9d47984f
Mamadou Dior DIAW <mamadou.diaw@sogem-omvs.org>
2022-08-03 14:36:50 +0000

    home recipes

--- src/components/home.js
+++ src/components/home.js
@@ -1,19 +1,24 @@
 import { useState } from "react";
+import img1 from '../images/image1.png';
+import img2 from '../images/image2.png';
+import img3 from '../images/image3.png';
+import img4 from '../images/image4.png';
 
 
 function Home() {
-   
+
     const [toggle, setToggle] = useState(false);
 
     const hamburgerMenu = () => {
         setToggle(!toggle)
     }
-   
+
     return (
-        <nav className="navbar">
-            <h3 className="logo">logo</h3>
-            <h2 className="toggle"><i className={toggle ? 'fas fa-times' : 'fas fa-bars'} onClick={hamburgerMenu}></i></h2>
-                <ul className={toggle? 'navToggled': 'nav'}>
+        <>
+            <nav className="navbar">
+                <h3 className="logo">logo</h3>
+                <h2 className="toggle"><i className={toggle ? "fa-solid fa-times" : "fa-solid fa-bars"} onClick={hamburgerMenu}></i></h2>
+                <ul className={toggle ? 'navToggled' : 'nav'} onClick={() => setToggle(false)}>
                     <li className="item">
                         <a href="/accueil">Breakfast</a>
                     </li>
@@ -25,10 +30,23 @@ function Home() {
                     </li>
                     <li className="item">
                         <a href="/accueil">login</a>
-                    </li> 
+                    </li>
                 </ul>
-           
-        </nav>
+
+            </nav>
+            <div className="ellipse">
+                <img className="image1" src={img1} alt="" />
+                <img className="image2" src={img2} alt="" />
+                <img className="image3" src={img3} alt="" />
+                <img className="image4" src={img4} alt="" />
+
+            </div>
+
+
+
+
+        </>
+
 
 
     );
